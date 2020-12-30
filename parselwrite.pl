61eba05a9160d0afab25f275a05f6c456fc74291
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-30 20:41:35 +0100

    calculate cmd length

--- parselwrite.pl
+++ parselwrite.pl
@@ -1,15 +1,27 @@
 #!/usr/bin/perl
 
-
-my $string = shift @ARGV;
-
 my $device="/dev/hidraw0" ;
+
+
+my ($cmd, $cnt);
+
+if ($#ARGV == 0) {
+ $cnt  = shift @ARGV;
+ $cmd = 'P' ;
+} elsif ($#ARGV == 1) {
+  ($cmd , $cnt) = @ARGV;
+} else {
+	die "usage: $0 [cmd = S|P] [content] ";
+}
+
+my $string = (sprintf  "^%1s%03d%s", $cmd, (length ($cnt) +1), $cnt) ;
+
 
 open HID, ">" . $device or die "cannot open $device - reason: $!" ;
 
 
-printf   "^%s|\n", $string;
-printf HID  "^%s\r", $string;
+printf   "%s|\n", $string;
+printf HID  "%s\r", $string;
 
 
 close HID;

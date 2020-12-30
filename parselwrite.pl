d88c6aa37f31f625a9e87e663dcfcfe64a86766d
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-31 00:00:25 +0100

    timed print - does not help

--- parselwrite.pl
+++ parselwrite.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+use Data::Dumper ;
+use Time::HiRes ;
+
 my $device="/dev/hidraw0" ;
 
 
@@ -21,9 +24,42 @@ open HID, ">" . $device or die "cannot open $device - reason: $!" ;
 
 
 printf   "%s|\n", $string;
-printf HID  "%s\r", $string;
+# printf HID  "%s\r", $string;
+parsel_print ($string) ;
 
 
 close HID;
 
+exit;
+
+#~~~~~~~~~~~~~~~~~~~~
+#
+# usb chunked_print (handle, string)
+# don'oveerrun 2400 baud processor
+#
+sub parsel_print {
+  my $str = shift @_;
+  # $str .= "\r";
+  # while ($my chunk = substr($str, 
+  my @chunks = unpack("(A4)*", $str);
+  push @chunks, "\r";
+  print Dumper (@chunks);
+  my $timestamp = Time::HiRes::time();
+  foreach my $chunk (@chunks) {
+	  # my $nexdue = Time::HiRes::time() + 0.04 ; # wait one 
+	  # Time::HiRes::usleep (max(0, $timestamp +0.05));
+	  
+	  my $timediff = 0.3 + ($timestamp - Time::HiRes::time())  ;
+	  printf " >%f< ", $timediff ;
+	  if ($timediff > 0) { print "#" ; Time::HiRes::sleep($timediff ) } ;
+	  sleep(1) ;
+	  # $timestamp = Time::HiRes::time() ;
+	  print $chunk, '|'; 
+	  print HID  $chunk;
+
+	  $timestamp = Time::HiRes::time() ;
+  }
+  print "\n" ;
+  # die "======= DEBUG =========";
+}
 

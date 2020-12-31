1b8e8373d47f500e6dc926939d8900e5a98748ee
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-31 19:36:09 +0100

    still playing...

--- parselread.pl
+++ parselread.pl
@@ -10,7 +10,7 @@ $/ = "\r"  ; # set line deli,iter to CR
 
 while (<HID>) {
 
-	print $_ , "\n" ;
+	print ( substr ($_ , 0,-2  ), "\n" ) ;
 
 }
 
--- parselw_USB.pl
+++ parselw_USB.pl
@@ -2,10 +2,10 @@
 
 use Data::Dumper ;
 use Time::HiRes ;
+use USB::LibUSB;
 
-# my $device="/dev/hidraw0" ;
 
-my $device="../dev_infini_serial" ;
+my $device="/dev/hidraw0" ;
 
 # $| = 1; # don't buffer output
 
@@ -23,13 +23,21 @@ if ($#ARGV == 0) {
 my $string = (sprintf  "^%1s%03d%s", $cmd, (length ($cnt) +1), $cnt) ;
 
 
-open HID, ">" . $device or die "cannot open $device - reason: $!" ;
+# open HID, ">" . $device or die "cannot open $device - reason: $!" ;
 
 # HID->autoflush(1);
 
-# printf   "%s|\n", $string;
-printf HID  "%s\r", $string;
-# parsel_print ($string) ;
+my $ctx = USB::LibUSB->init();
+#  idVendor=0665, idProduct=5161
+my $handle = $ctx->open_device_with_vid_pid( 0x0665, 0x5161 );
+
+$handle->set_auto_detach_kernel_driver(1);
+$handle->claim_interface(0);
+
+
+printf   "%s|\n", $string;
+# printf HID  "%s\r", $string;
+parsel_print ( $handle ,  $string ) ;
 
 
 close HID;
@@ -42,7 +50,7 @@ exit;
 # don'oveerrun 2400 baud processor
 #
 sub parsel_print {
-  my $str = shift @_;
+  my ($hdl, $str) =  @_;
   # $str .= "\r";
   # while ($my chunk = substr($str, 
   my @chunks = unpack("(A4)*", $str);
@@ -59,9 +67,9 @@ sub parsel_print {
 	  # sleep(1) ;
 	  # $timestamp = Time::HiRes::time() ;
 	  print $chunk, '|'; 
-	  print HID  $chunk;
+	  # print HID  $chunk;
 	  # syswrite HID , $chunk;
-
+          $hdl->control_transfer_write(0x21, 0x9, 0x200, 0, $chunk);
 	  $timestamp = Time::HiRes::time() ;
   }
   print "\n" ;

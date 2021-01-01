70aab9dc993db4f6a47e7c9278e507d4120830ac
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-01 12:38:27 +0100

    cleanup

--- parselask.pl
+++ parselask.pl
@@ -3,11 +3,7 @@
 use Data::Dumper ;
 use Time::HiRes ;
 
-# my $device="/dev/hidraw0" ;
-
 my $device="../dev_infini_serial" ;
-
-# $| = 1; # don't buffer output
 
 my ($cmd, $cnt);
 
@@ -23,51 +19,17 @@ if ($#ARGV == 0) {
 my $string = (sprintf  "^%1s%03d%s", $cmd, (length ($cnt) +1), $cnt) ;
 
 
-open HID, "+<" . $device or die "cannot open $device - reason: $!" ;
+open my $HID, "+<" . $device or die "cannot open $device - reason: $!" ;
 $/ = "\r" ;
 
-# HID->autoflush(1);
 
-# printf   "%s|\n", $string;
-printf HID  "%s\r", $string;
-# parsel_print ($string) ;
+printf $HID  "%s\r", $string;
 
-my $response=<HID>;
+my $response=<$HID>;
 print ( substr ($response , 0,-3  ), "\n" ) ;
 
-close HID;
+close $HID;
 
 exit;
 
-#~~~~~~~~~~~~~~~~~~~~
-#
-# usb chunked_print (handle, string)
-# don'oveerrun 2400 baud processor
-#
-sub parsel_print {
-  my $str = shift @_;
-  # $str .= "\r";
-  # while ($my chunk = substr($str, 
-  my @chunks = unpack("(A4)*", $str);
-  push @chunks, "\r";
-  print Dumper (@chunks);
-  my $timestamp = Time::HiRes::time();
-  foreach my $chunk (@chunks) {
-	  # my $nexdue = Time::HiRes::time() + 0.04 ; # wait one 
-	  # Time::HiRes::usleep (max(0, $timestamp +0.05));
-	  
-	  my $timediff = 0.05 + ($timestamp - Time::HiRes::time())  ;
-	  printf " >%f< ", $timediff ;
-	  if ($timediff > 0) { print "#" ; Time::HiRes::sleep($timediff ) } ;
-	  # sleep(1) ;
-	  # $timestamp = Time::HiRes::time() ;
-	  print $chunk, '|'; 
-	  print HID  $chunk;
-	  # syswrite HID , $chunk;
-
-	  $timestamp = Time::HiRes::time() ;
-  }
-  print "\n" ;
-  # die "======= DEBUG =========";
-}
 

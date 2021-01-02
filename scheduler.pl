1e67f5081cd196ea622edb64a4e4c1b6e4ec3c02
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-02 17:42:50 +0100

    collecting some subs

--- scheduler.pl
+++ scheduler.pl
@@ -30,20 +30,22 @@ our @rrd_def;
 require ('./P17_def.pl');
 
 unlink ( $infini_cmd_send_pipe, $infini_cmd_read_pipe);
-POSIX::mkfifo("$infini_cmd_send_pipe", 0666) or die "canot create fifo $infini_cmd_send_pipe: $!";
-POSIX::mkfifo("$infini_cmd_read_pipe", 0666) or die "canot create fifo $infini_cmd_read_pipe: $!";
+POSIX::mkfifo("$infini_cmd_send_pipe", 0666) or die "canot create fifo $infini_cmd_send_pipe : $!";
+POSIX::mkfifo("$infini_cmd_read_pipe", 0666) or die "canot create fifo $infini_cmd_read_pipe : $!";
 # print "fifos created ...\n";
+
+our $INFINI = POSIX::open( $infini_device ) or die "canot open $infini_device : $!";
 
 our $SEND_PIPE = POSIX::open($infini_cmd_send_pipe,  
 	&POSIX::O_RDONLY | &POSIX::O_NONBLOCK ) 
-	or die "cannot open socket $infini_cmd_send_pipe: $!";
+	or die "cannot open socket $infini_cmd_send_pipe : $!";
 # print "send pipe open\n";
 
 # open(our $READ_PIPE, '>>', $infini_cmd_read_pipe ) or die "cannot open socket $infini_cmd_send_pipe: $!";
 
 our $READ_PIPE = POSIX::open($infini_cmd_read_pipe,  
 	&POSIX::O_NONBLOCK  ) 
-	or die "cannot open socket $infini_cmd_read_pipe: $!";
+	or die "cannot open socket $infini_cmd_read_pipe : $!";
 # print "read pipe open\n";
 
 
@@ -51,9 +53,51 @@ our $READ_PIPE = POSIX::open($infini_cmd_read_pipe,
 
 POSIX::close $SEND_PIPE; 
 POSIX::close $READ_PIPE;
-
+POSIX::close $INFINI;
 
 exit;
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # subs....
 
+# $response = call_infini_raw ($request)
+sub call_infini_raw {
+  my $qrys = shift @_;
+  printf $INFINI  "%s\r", $qrys;
+  my $rv=<$INFINI>;
+  return $rv;
+}
+
+# ($flag, $len, $valptr) = call_infini_cooked ( $content, [$command] )
+sub call_infini_cooked {
+  my $qry = compose_qry (@_);
+  my $rsp = call_infini_raw($qry);
+  return (checkstring( $rsp ));
+}
+
+# $query = compose_qry ( $content, [$command] )
+sub compose_qry {
+  my ($cnt, $cmd) = @_;
+  ($cmd) or $cmd = 'P';
+  my $qry = (sprintf  "^%1s%03d%s", $cmd, (length ($cnt) +1), $cnt) ;
+  return $qry;
+}
+
+# ($flag, $len, $valptr) = checkstring ($response );
+sub checkstring {
+  my $resp = shift @_;
+
+  my $resp1 = substr ($resp , 0,-3  );
+  my $crc = substr ($resp , -3,2  );
+
+  ( my ($label, $len, $payload) = 
+	  ($resp1 =~ /\^(\w)(\d{3})(.*)$/) )  
+	  or return (0) ;
+
+  return (0) if ( length($resp)-5-$len ) ;
+
+  my $digest = crc($resp1, 16, 0x0000, 0x0000, 0 , 0x1021, 0, 1); 
+  return (0) unless $digest == unpack ('n', $crc  )  ;
+
+  my @data = split(',', $payload);
+  return ($label, $len,  \@data  );
+}

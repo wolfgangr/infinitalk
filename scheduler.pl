08b416a4b64eefaef814cb0bfcd6f63b4e1610f4
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-03 01:03:42 +0100

    compose rrd_stat_tpl

--- scheduler.pl
+++ scheduler.pl
@@ -59,8 +59,12 @@ foreach  my $dl (@rrd_def) {
 
 our @rrd_cmd_list = sort keys %rrd_def_by_cmd;
 
-debug_dumper(5, \%rrd_def_by_label , \%rrd_def_by_cmd, \@rrd_cmd_list );
+our $rrd_stat_tpl = join(':', map { $$_[0] } @rrd_def) ;
 
+debug_dumper(5, \%rrd_def_by_label , \%rrd_def_by_cmd, \@rrd_cmd_list, \@rrd_def );
+debug_print (5, "rrd_stat_tpl = \n\t$rrd_stat_tpl \n");
+
+die "### debug ---- setup-----  #####";
 
 # ---- prepare file handlers ------
 
@@ -155,6 +159,8 @@ sub stat_iterator {
   # $s_counter++ >= 4 and $s_counter=0;
   if ( $s_counter++ >= $#rrd_cmd_list) {
     debug_dumper ( 5, \%res , \@rrd_def) ;
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     die "#### debug in  # stat_iterator ####";
   }
 

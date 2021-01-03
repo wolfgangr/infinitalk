d8f74e3e6bcf5167bd2055a0d704306b03260eea
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-03 22:35:05 +0100

    crafting watchdog

---
+++ watchdog.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd ~wrosner/infini/parsel
+PROCESS='scheduler.pl'
+
+STARTER="./$PROCESS"
+PRESTARTER='./setstty-RS485.sh' 
+CALLER='/usr/bin/perl'
+LOGFILE='/var/log/wrosner/watchdog_infini.log'
+UPDLOG='/var/log/wrosner/infini_scheduler.log'
+
+# uncomment this 2 line for debug
+# echo -n "chargery rrd watchdog entered " >> $LOGFILE
+# date >> $LOGFILE
+
+
+# exit - nothing to do if rrdtest reports success
+./rrdtest.pl *.rrd   2>> $LOGFILE | tail -n1 >> $LOGFILE 
+STATUS=${PIPESTATUS[0]}
+if [ $STATUS -eq 0 ] ; then
+	exit
+fi
+
+
+
+echo -n "infini rrd watchdog triggered at " >> $LOGFILE
+date >> $LOGFILE
+
+ps ax | grep "./$PROCESS" | grep '/usr/bin/perl' >> $LOGFILE
+
+killall $PROCESS  >>  $LOGFILE 2>&1
+sleep 1 
+killall -9 $PROCESS  >> $LOGFILE 2>&1
+sleep 1
+
+$PRESTARTER  >> $LOGFILE 2>&1
+$STARTER   >> $UPDLOG 2>&1
+
+

4a1bb1a516110298150621f250d148ffefafcf33
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-04 20:02:17 +0100

    try to figure out ftok stuff

--- mqsv-tester.pl
+++ mqsv-tester.pl
@@ -4,20 +4,25 @@ use strict;
 use warnings;
 
 use IPC::SysV qw(IPC_PRIVATE S_IRUSR S_IWUSR ftok IPC_CREAT  );
-use IPC::Msg;
+use IPC::Msg();
+use Cwd qw( realpath );
 # use Fcntl;
 
 use Data::Dumper qw (Dumper) ;
 
-
-my $path = `pwd` . '/' . '$0' ;
-printf "%s\n", $path ;  
+# my $pwd = `pwd`;
+# chomp $pwd;
+# $pwd ='';
+# my $path = $pwd .  "/$0" ;
+my $path = realpath ($0)  ;
+# printf "%s\n", $path ;  
 # $path = "/home/wrosner/infini/parsel/qsv-tester.pl" ;
-$path = "/home/wrosner/infini/parsel/tmp/test.mq" ;
-my $ftok_foo = ftok( $path  );
-my $ftok_bar = ftok( $path   );
+# $path = "/home/wrosner/infini/parsel/tmp/test.mq" ;
+my $ftok_foo = ftok ( realpath ($0),   'f' );
+my $ftok_bar = ftok ( realpath ($0),  'b' );
+my $ftok_def = ftok ( realpath ($0) ); 
 
-printf "ftoks von foo: 0x%x , bar: 0x%x in path %s \n", $ftok_foo, $ftok_bar, $path ;
+printf "ftoks von foo: 0x%08x , bar: 0x%08x , default 0x%08x in path %s \n", $ftok_foo, $ftok_bar, $ftok_def, $path ;
 
 
 my $msg ;
@@ -46,4 +51,14 @@ while (1) {};
 
 $msg->remove;
 
+exit 1;
 
+# ~~~~~~~~~~~~~~~~~~~
+# return a ftok based on some key an our own script pathname
+sub my_ftok{ 
+  my $id = shift || 1 ;
+  my $pwd = `pwd`;
+  chomp $pwd;
+
+  return ftok(spritf ("%s/%s", $pwd, $0), $id )
+}

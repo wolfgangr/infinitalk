1fb3aef1dda9e5c09fe4b24ba15591e9e158d4d2
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-05 09:31:48 +0100

    can retrieve arbitrary commands via mq from cmdline

--- mqsv-parselask.pl
+++ mqsv-parselask.pl
@@ -7,28 +7,40 @@ use IPC::SysV qw(IPC_PRIVATE S_IRUSR S_IWUSR ftok IPC_CREAT IPC_NOWAIT );
 use IPC::Msg();
 use Cwd qw( realpath );
 use Time::HiRes () ;
+use Digest::CRC qw(crc) ;
 
+# -- process command line args
+my ($cmd, $cnt);
 
+if ($#ARGV == 0) {
+ $cnt  = shift @ARGV;
+ $cmd = 'P' ;
+} elsif ($#ARGV == 1) {
+  ($cmd , $cnt) = @ARGV;
+} else {
+	die "usage: $0 [cmd = S|P] [content] ";
+}
+
+# -- mq configuration 
 my $ftokid = 1;
 my $server = './scheduler.pl';
-my $ftok_server = ftok ( my_realpath ($server) );
-my $ftok_my = ftok ( my_realpath ($0) );
+my $ftok_server = ftok ( realpath ($server) );
+my $ftok_my = ftok ( realpath ($0) );
 
-printf "\$0: %s, realpath: %s \n", $0, realpath ($0);
+# printf "\$0: %s, realpath: %s \n", $0, realpath ($0);
 
 
 my $mq_srv = IPC::Msg->new($ftok_server ,  S_IWUSR | S_IRUSR |  IPC_CREAT )
 	 or die sprintf ( "cant create server mq using token >0x%08x< ", $ftok_server ); 
 my $mq_my  = IPC::Msg->new($ftok_my     ,  S_IWUSR | S_IRUSR |  IPC_CREAT )
-	or die sprintf ( "cant create server mq using token >0x%08x< ", $ftok_my  );
+	or die sprintf ( "cant create client mq using token >0x%08x< ", $ftok_my  );
 
-print "setup done \n";
+# print "setup done \n";
 
-my $cnt =1;
 do {
   # rolling ms
-  my $ts = (int (Time::HiRes::time * 1000)) % 0x100000000 ;
-  my $msg = sprintf ("%08x:%08x::GOV" , $ftok_my, $ts );
+  my $ts = (int (Time::HiRes::time * 1000)) & 0xffffffff ;
+  my $msg = sprintf ("%08x:%08x:%s:%s" , $ftok_my, $ts, $cmd , $cnt );
   print "sending .... ", $msg ;
   $mq_srv->snd (1, $msg );
   print " ... done \n";
@@ -41,20 +53,12 @@ do {
   print $buf , "\n" if $buf  ;
 
   # sleep 4;
-  $cnt++;
 
 
-} until (1);
+} until (1);  # yes, this was a loop tester before
 
 exit 1;
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-sub my_realpath {
-  my $p = shift ;
-  my $pwd = `pwd`;
-  chomp $pwd;
-  return sprintf ( "%s/%s", $pwd, $p );
-
-}

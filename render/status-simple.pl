a71602507c6ac28ec365c1d1a4bd9296dc2a1cd0
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-23 09:46:33 +0100

    read protocol definition

--- render/status-simple.pl
+++ render/status-simple.pl
@@ -1,24 +1,50 @@
 #!/usr/bin/perl
+
+# simple web render of infini status caches in tmp/*.bck
+
 use strict;
 use warnings;
+
 use CGI () ; #  qw/:standard/;
 use Data::Dumper::Simple ;
 use Time::Piece;
+use Storable();
 use utf8 ;
+
 
 my $dtformat = '%F - %T' ;
 my $title = "Infini status";
 my $status_files = `ls -1 ~wrosner/infini/parsel/tmp/*.bck` ;
+
+our %p17 = ();
+require '../P17_def.pl' ;
 
 #~~~~~~~~ end of config ~~~~~
 
 my $tp_now = Time::Piece->new() ;
 $title .= ' - ' . $tp_now->strftime($dtformat) ;
 
+# parse file name list
 my @stat_fls =  sort split "\n", $status_files;
+my %status = 
+	map { 
+		/\/(\w+)\..+$/   ; 
+		my %h = ( tag => $1 , path => $_ ) ;
+		($1 , \%h ) 
+	} 
+	@stat_fls;
+	# split "\n", $status_files;
 
-my %status = map { /\/(\w+)\..+$/   ; ($1 , $_ ) } @stat_fls ;
-
+# read from disk
+for my $sfh (values %status ) {
+	next unless $sfh->{ exists } = -e $sfh->{  path }  ;
+	# $sfh->{ kilroy } = 'was here' ;
+	# content
+	$sfh->{ ihash } = Storable::lock_retrieve( $sfh->{  path } );
+	# update time
+	$sfh->{ upd_t } = (stat( $sfh->{  path }   ))[9];
+	$sfh->{ read } = 1 ;
+}
 
 # ~~~~~~~~~~~~ generate HTML ~~~~~~~~~
 
@@ -28,13 +54,13 @@ print CGI::header();
 print CGI::start_html(-title => $title);
 print CGI::h3($title);
 
-print join "<br>\n", @stat_fls ;
+print join "<br>\n",  @stat_fls;
 print "<br>\n" ;
 
 print CGI::h2('Debug:');
 print "<pre>";
 
-print Dumper( %status , @stat_fls);
+print Dumper( @stat_fls, %status, %p17  );
 
 print "</pre>";
 

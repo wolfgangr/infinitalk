dc252381f666829e4adeafb3f1a62a77fa7e359c
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-23 22:04:04 +0100

    playing text/plain

--- render/loggrep.pl
+++ render/loggrep.pl
@@ -15,13 +15,43 @@ use warnings;
 use strict;
 use CGI();
 use Time::Piece();
+use Data::Dumper::Simple;
 
 my $logfile = './infini-status.log' ;
 
 my $q=CGI->new;
+print $q->header(-type => 'text/plain' ,
+	-charset => 'utf8' );
+
+my $from  = $q->param('from' );
+my $until = $q->param('until');
+
+# print $q->header(-type => 'text/cs',
+# 		-title => 'infini log file export',
+# 		-encoding => 'utf8' 
+# );
+# print $q->header(-type => 'text/csv');
+print $q->header(-type => 'text/plain'); 
+
+open ( my $LOG , '<', $logfile ) or die "cannot open $logfile : $!"; 
+
+while (<$LOG>) {
+	# 2021-01-21 15:08:53 - status chaged old->new:##,####,######:05,4541,000003
+	my @fields = /^([\d\-]{10} [\d\:]{8})[^:]*:([\d\#\,]{14}):([\d\,]{14})$/ ;
+
+	next unless ( (scalar @fields) == 3) ;
+	my $dt_line = Time::Piece->new( $fields[0]) ; 
+
+	print Dumper ( @fields, $dt_line);
+	last;
+	#===============================
+}
+close $LOG ;
+
+# DEBUG ($q, $from , $until ) ;
+print "~~~~~~~~~~~~~<br><hr>END\n";
 
 
-DEBUG ($q) ;
 
 
 exit;
@@ -29,10 +59,10 @@ exit;
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # debug with continued laoding
 sub debug {
-  my $in = shift;
+  # my $in = shift;
   print
     "\n<pre><code>\n",
-    $in,
+    Dumper(@_);
     "\</code></pre>\n",
   ;
 }
@@ -40,11 +70,12 @@ sub debug {
 
 # final die like debug
 sub DEBUG {
-    my $in = shift;
-    print CG::header;
+    # my $in = shift;
+    print CGI::header;
     print CGI::start_html('### DEBUG ###');
-    debug ( $in) ;
+    debug ( @_  ) ;
     print CGI::end_html;
   ;
-  exit; # is it bad habit to exit from a sum??
+  exit; # if we prefer not to clobber the log file
+  # die " ============ hit the wall ========= ";
 }
